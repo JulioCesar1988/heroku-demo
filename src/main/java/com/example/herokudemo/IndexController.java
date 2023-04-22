f8eea9774fd3a65eb30aee9270927d9eb8bb72c6
JulioCesar1988 <julio.unlp2010@gmail.com>
2023-04-22 19:34:17 -0300

    nueva pantalla

--- src/main/java/com/example/herokudemo/IndexController.java
+++ src/main/java/com/example/herokudemo/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class IndexController {
     @GetMapping("/")
     public String index() {
-        return "login";
+        return "home";
     }
 
 
