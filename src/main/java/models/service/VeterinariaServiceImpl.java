1cbd1462926285b3e15533bd445ac469110ca8d1
JulioCesar1988 <julio.unlp2010@gmail.com>
2023-04-22 23:35:44 -0300

    v

--- src/main/java/models/service/VeterinariaServiceImpl.java
+++ src/main/java/models/service/VeterinariaServiceImpl.java
@@ -1,6 +1,7 @@
 package models.service;
 
 
+import models.dao.IVeterinariaDao;
 import models.entity.Veterinaria;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
