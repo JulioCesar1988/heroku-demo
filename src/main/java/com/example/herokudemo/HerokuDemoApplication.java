d9f172775d5c49466ebeec5cd3ca7cec2306bf0e
JulioCesar1988 <julio.unlp2010@gmail.com>
2023-04-23 08:35:53 -0300

    scanear controllers

--- src/main/java/com/example/herokudemo/HerokuDemoApplication.java
+++ src/main/java/com/example/herokudemo/HerokuDemoApplication.java
@@ -2,7 +2,10 @@ package com.example.herokudemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
+
+@ComponentScan({"controllers"})
 @SpringBootApplication
 public class HerokuDemoApplication {
 
