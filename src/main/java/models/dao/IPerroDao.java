570715b8f98947c51d972a9c1bd18524ac0b557a
JulioCesar1988 <julio.unlp2010@gmail.com>
2023-05-05 17:33:34 -0300

    cambios por temas de insert

--- src/main/java/models/dao/IPerroDao.java
+++
@@ -1,8 +0,0 @@
-package models.dao;
-
-
-import models.entity.Perro;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IPerroDao extends CrudRepository<Perro, Long> {
-}
